d0afaed53bb1ebd576838fc52f0501aa971ebc66
ChrisKujn <157987691+ChrisKujn@users.noreply.github.com>
2024-02-08 19:29:29 +0100

    Update Projekt Daten Analyse.py

--- Projekt Daten Analyse.py
+++ Projekt Daten Analyse.py
@@ -3,13 +3,15 @@ nltk.download('stopwords')
 import re 
 import pandas as pd
 import numpy as np
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
+
 import nltk
 import ntlk.corpus 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 
 
 # Inhalt der CSV 'Comcast.csv' lesen / Data Frame (df) = pandas.read.csv
@@ -25,7 +27,7 @@ df['Customer Complaint'] = df['Customer Complaint'].apply(word_tokenize)
 
 # eStopWords mit den englischen Stoppwörtern füllen, da der vorhandene Datensatz auf englisch ist 
 eStopWords = set(stopwords.words('english'))
-eStopWords.add('comcast')
+eStopWords.add('comcast') #Comcast als zusätzliches Stopwort, da dieses sehr häufig in den Beschwerden auftaucht aber keine relevante Bedeutung hat.
 
 # Bedeutungslose Wörter, auch Stoppwörter genannt, entfernen.
 df['Customer Complaint'] = df['Customer Complaint'].apply(lambda x: [word for word in x if word not in eStopWords])
@@ -48,7 +50,13 @@ X = vectorizer.fit_transform(df['Customer Complaint'].apply(' '.join))
 tfidf = TfidfVectorizer()
 X_tfidf = tfidf.fit_transform(df['Customer Complaint'].apply(' '.join))
 
+# LSA
+lsa = TruncatedSVD(n_components=4, algorithm='randomized', n_iter=15, random_state=42)
+lsa_output = lsa.fit_transform(X)
+# Neue Spalte für jede Komponente im DF
+for i in range(lsa_output.shape[1]):
+    df[f'LSA Topic {i}'] = lsa_output[:, i]
 
 # Coherence Score (Bestimmung der Anzahl von Themen)
 # Word Embeddings oder zumindest N-Gram
-# ggf. Custom Stoppwortfilter (sogenannte Domain-Filter)
+
