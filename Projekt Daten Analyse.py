6b070142b02247a805b783089041e014898ba9f1
ChrisKujn <157987691+ChrisKujn@users.noreply.github.com>
2024-02-09 21:35:17 +0100

    Update Projekt Daten Analyse.py

--- Projekt Daten Analyse.py
+++ Projekt Daten Analyse.py
@@ -13,6 +13,9 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
+from gensim.models import CoherenceModel
+from gensim.corpora import Dictionary
+
 
 # Inhalt der CSV 'Comcast.csv' lesen / Data Frame (df) = pandas.read.csv
 df = pd.read.csv ('Comcast.csv', engine = 'python')
@@ -50,6 +53,8 @@ X = vectorizer.fit_transform(df['Customer Complaint'].apply(' '.join))
 tfidf = TfidfVectorizer()
 X_tfidf = tfidf.fit_transform(df['Customer Complaint'].apply(' '.join))
 
+
+
 # LSA
 lsa = TruncatedSVD(n_components=4, algorithm='randomized', n_iter=15, random_state=42)
 lsa_output = lsa.fit_transform(X)
@@ -57,6 +62,37 @@ lsa_output = lsa.fit_transform(X)
 for i in range(lsa_output.shape[1]):
     df[f'LSA Topic {i}'] = lsa_output[:, i]
 
+
+# LDA
+lda = LatentDirichletAllocation(n_components=3, doc_topic_prior=0.9, topic_word_prior=0.9)
+lda_output = lda.fit_transform(X)
+# Neue Spalte für jede Komponente im DF
+for i in range(lda_output.shape[1]):
+    df[f'LDA Topic {i}'] = lda_output[:, i]
+
+# Verzeichnis für die Themen erstellen
+dictionary = Dictionary(df['Customer Complaint'])
+
+# Umwandlung in eine vektorisierte Form durch Berechnung des "Frequency counts"
+corpus = [dictionary.doc2bow(doc) for doc in df['Customer Complaint']]
+
+# Themen extrahieren
+n_top_words = 3 
+topics = []
+feature_names = vectorizer.get_feature_names_out()
+
+for topic_idx, topic in enumerate(lda.components_):
+    top_features_ind = topic.argsort()[:-n_top_words - 1:-1]
+    top_features = [feature_names[i] for i in top_features_ind]
+    topics.append(top_features)
+
 # Coherence Score (Bestimmung der Anzahl von Themen)
-# Word Embeddings oder zumindest N-Gram
+coherence_model_lda = CoherenceModel(topics=topics, texts=df['Customer Complaint'], dictionary=dictionary, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+
+print('Coherence Score: ', coherence_lda)
+print(topics)
+#LDA Themen
+for i, topic in enumerate(topics):
+    print(f"Top words for topic {i}: {', '.join(topic)}")
 
